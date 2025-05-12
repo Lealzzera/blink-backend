bc212d519ce7c27bf024ceeeab4f334ea2c27377
Nicolas Sinegalia <sinegalia.nicolas@gmail.com>
2025-05-11 23:57:58 -0300

    Starting Blink backend project

---
+++ src/main/java/com/blink/blink_backend/BlinkBackendApplication.java
@@ -0,0 +1,16 @@
+package com.blink.blink_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BlinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlinkBackendApplication.class, args);
+	}
+
+
+
+}
