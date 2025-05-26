af95b39006efecf9b9dd4172c34a9646f0462de3
Nicolas Sinegalia <sinegalia.nicolas@gmail.com>
2025-05-26 17:43:18 -0300

    Novo endpoint updateAvailabilityConfiguration

---
+++ src/main/java/com/blink/backend/controller/configuration/ClinicConfigurationController.java
@@ -0,0 +1,31 @@
+package com.blink.backend.controller.configuration;
+
+
+import com.blink.backend.controller.configuration.dto.AvailabilityConfigurationDTO;
+import com.blink.backend.domain.service.ConfigurationAvailabilityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("configurations")
+public class ClinicConfigurationController {
+
+    private ConfigurationAvailabilityService configurationAvailabilityService;
+
+    @PutMapping("/configurations/availability")
+    public ResponseEntity<Void> updateAvailabilityConfiguration(@RequestBody List<AvailabilityConfigurationDTO> updateAvailabilityConfiguration){
+
+        configurationAvailabilityService.updateAvailabilityConfiguration(updateAvailabilityConfiguration);
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+}
