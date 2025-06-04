3851e6851e32509495cb7a8aca46fb3617c9fd43
Gustavo Macias Correa <gu.mcorrea@gmail.com>
2025-06-04 13:37:16 -0300

    Ajustando endpoint quebrado

--- src/main/java/com/blink/backend/controller/configuration/ClinicConfigurationController.java
+++ src/main/java/com/blink/backend/controller/configuration/ClinicConfigurationController.java
@@ -7,10 +7,10 @@ import com.blink.backend.domain.service.ClinicConfigurationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,36 +19,27 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("configurations")
 public class ClinicConfigurationController {
-
     private final ClinicConfigurationService clinicConfigurationService;
 
     @PutMapping("availability")
     public ResponseEntity<Void> updateAvailabilityConfiguration(@RequestBody List<AvailabilityConfigurationDTO> updateAvailabilityConfiguration) {
-
         clinicConfigurationService.updateAvailabilityConfiguration(updateAvailabilityConfiguration);
-
         return ResponseEntity.noContent().build();
-
     }
 
     @PutMapping("appointments")
     public ResponseEntity<Void> updateAppointmentConfiguration(@RequestBody AppointmentConfigurationDTO appointmentConfiguration) {
-
         clinicConfigurationService.updateAppointmentConfiguration(appointmentConfiguration);
-
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("availability/{clinicId}")
-    public ResponseEntity<List<AvailabilityConfigurationDTO>> getAvailabilityConfiguration(@RequestParam Integer clinicId) {
-
+    public ResponseEntity<List<AvailabilityConfigurationDTO>> getAvailabilityConfiguration(@PathVariable Integer clinicId) {
         return ResponseEntity.ok(clinicConfigurationService.getAvailabilityConfiguration(clinicId));
     }
 
     @GetMapping("appointments/{clinicId}")
-    public ResponseEntity<AppointmentConfigurationDTO> getAppointmentConfiguration(@RequestParam Integer clinicId){
-
+    public ResponseEntity<AppointmentConfigurationDTO> getAppointmentConfiguration(@PathVariable Integer clinicId){
         return ResponseEntity.ok(clinicConfigurationService.getAppointmentConfiguration(clinicId));
-
     }
 }
