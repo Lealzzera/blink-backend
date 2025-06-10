130f0de878c4cacb7302e51976e1fb8ef1e90bfe
Gustavo Macias Correa <gu.mcorrea@gmail.com>
2025-06-09 23:27:19 -0300

    corrigindo imports

--- src/main/java/com/blink/backend/domain/service/ClinicAvailabilityService.java
+++ src/main/java/com/blink/backend/domain/service/ClinicAvailabilityService.java
@@ -26,9 +26,11 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
-import static com.blink.backend.domain.exception.appointment.AppointmentConflictException.AppointmentConflitReason.*;
+import static com.blink.backend.domain.exception.appointment.AppointmentConflictException.AppointmentConflitReason.DURING_BREAK;
+import static com.blink.backend.domain.exception.appointment.AppointmentConflictException.AppointmentConflitReason.OUTSIDE_WORK_DAY;
+import static com.blink.backend.domain.exception.appointment.AppointmentConflictException.AppointmentConflitReason.OUTSIDE_WORK_HOURS;
+import static com.blink.backend.domain.exception.appointment.AppointmentConflictException.AppointmentConflitReason.OVERLAP;
 import static com.blink.backend.persistence.entity.appointment.AppointmentStatus.AGENDADO;
 
 @Service
