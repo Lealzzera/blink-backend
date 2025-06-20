fa31a288a92f4157a688ea194fb9de792a44fdec
Gustavo Macias Correa <gu.mcorrea@gmail.com>
2025-06-20 12:30:12 -0300

    Add constructor to sale

--- src/main/java/com/blink/backend/persistence/entity/appointment/Sale.java
+++ src/main/java/com/blink/backend/persistence/entity/appointment/Sale.java
@@ -10,8 +10,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
@@ -21,6 +23,8 @@ import java.time.LocalDateTime;
 @Getter
 @Entity
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "sale")
 public class Sale {
 
