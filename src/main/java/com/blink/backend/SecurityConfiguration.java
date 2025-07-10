3bb80dd4a2069ed28966be556dec61d2ecae0e80
gumacias <gu.mcorrea@gmail.com>
2025-07-10 12:04:34 -0300

    Formatando swagger para tipos de data

--- src/main/java/com/blink/backend/SecurityConfiguration.java
+++ src/main/java/com/blink/backend/SecurityConfiguration.java
@@ -3,13 +3,22 @@ package com.blink.backend;
 import com.blink.backend.interceptor.EndpointLoggingInterceptor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import io.swagger.v3.core.converter.AnnotatedType;
+import io.swagger.v3.core.converter.ModelConverter;
+import io.swagger.v3.core.converter.ModelConverterContext;
 import io.swagger.v3.core.jackson.ModelResolver;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.media.StringSchema;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Iterator;
 
 @Configuration
 @RequiredArgsConstructor
@@ -39,6 +48,22 @@ public class SecurityConfiguration {
 
     @Bean
     public ModelResolver modelResolver(ObjectMapper objectMapper) {
-        return new ModelResolver(objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE));
+        return new ModelResolver(objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)){
+            @Override
+            public Schema<?> resolve(AnnotatedType type, ModelConverterContext context, Iterator<ModelConverter> chain) {
+                if (type.getType() == LocalDateTime.class) {
+                    return new StringSchema()
+                            .format("date-time")
+                            .example("2025-01-01 15:30")
+                            .description("yyyy-MM-dd HH:mm");
+                } else if (type.getType() == LocalTime.class) {
+                    return new StringSchema()
+                            .format("time")
+                            .example("09:00")
+                            .description("HH:mm");
+                }
+                return chain.hasNext() ? chain.next().resolve(type, context, chain) : null;
+            }
+        };
     }
 }
