c18abe0f35aeb27633fe06d754b051f1862bd4b6
Gustavo Macias Correa <gu.mcorrea@gmail.com>
2025-07-14 23:16:26 -0300

    rollback alteracoes

--- src/main/java/com/blink/backend/config/SecurityConfiguration.java
+++ src/main/java/com/blink/backend/config/SecurityConfiguration.java
@@ -1,24 +1,16 @@
-package com.blink.backend;
+package com.blink.backend.config;
 
 import com.blink.backend.interceptor.EndpointLoggingInterceptor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import io.swagger.v3.core.converter.AnnotatedType;
-import io.swagger.v3.core.converter.ModelConverter;
-import io.swagger.v3.core.converter.ModelConverterContext;
 import io.swagger.v3.core.jackson.ModelResolver;
-import io.swagger.v3.oas.models.media.Schema;
-import io.swagger.v3.oas.models.media.StringSchema;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.Iterator;
 
 @Configuration
 @RequiredArgsConstructor
@@ -30,7 +22,7 @@ public class SecurityConfiguration {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000", "https://localhost:3000", "https://be.blinkdentalmarketing.com.br")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
@@ -39,7 +31,7 @@ public class SecurityConfiguration {
             }
 
             @Override
-            public void addInterceptors(InterceptorRegistry registry) {
+            public void addInterceptors(@NonNull InterceptorRegistry registry) {
                 registry.addInterceptor(endpointLoggingInterceptor)
                         .addPathPatterns("/**");
             }
@@ -48,22 +40,6 @@ public class SecurityConfiguration {
 
     @Bean
     public ModelResolver modelResolver(ObjectMapper objectMapper) {
-        return new ModelResolver(objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)){
-            @Override
-            public Schema<?> resolve(AnnotatedType type, ModelConverterContext context, Iterator<ModelConverter> chain) {
-                if (type.getType() == LocalDateTime.class) {
-                    return new StringSchema()
-                            .format("string")
-                            .example("2025-01-01 15:30")
-                            .description("yyyy-MM-dd HH:mm");
-                } else if (type.getType() == LocalTime.class) {
-                    return new StringSchema()
-                            .format("string")
-                            .example("09:00")
-                            .description("HH:mm");
-                }
-                return chain.hasNext() ? chain.next().resolve(type, context, chain) : null;
-            }
-        };
+        return new ModelResolver(objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE));
     }
 }
