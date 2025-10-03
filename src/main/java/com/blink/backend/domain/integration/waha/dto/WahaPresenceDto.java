5a1d010229bd0a4fb5d68e2b6b8b57cf66839b5d
Gustavo Macias Correa <gu.mcorrea@gmail.com>
2025-10-03 16:06:06 -0300

    Waha config

--- src/main/java/com/blink/backend/domain/integration/waha/dto/WahaPresenceDto.java
+++ src/main/java/com/blink/backend/domain/integration/waha/dto/WahaPresenceDto.java
@@ -1,5 +1,7 @@
 package com.blink.backend.domain.integration.waha.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +9,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
 public class WahaPresenceDto {
     private String chatId;
     private String session;
