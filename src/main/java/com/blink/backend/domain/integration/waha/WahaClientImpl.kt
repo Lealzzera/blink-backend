deb50db8b926041ab9f9c89dc73dc629de19c5a2
Gustavo Macias Correa <gu.mcorrea@gmail.com>
2026-01-14 16:27:56 -0300

    more logs

--- src/main/java/com/blink/backend/domain/integration/waha/WahaClientImpl.kt
+++ src/main/java/com/blink/backend/domain/integration/waha/WahaClientImpl.kt
@@ -128,7 +128,7 @@ class WahaClientImpl(
                 session, chatId, limit, offset
             )
             .exchange { _, response ->
-                logger.info("Get messages completed, session=$session, chatId=$chatId, statusCode=${response.statusCode}, responseBody={}", response.body)
+                logger.info("Get messages completed, session=$session, chatId=$chatId, statusCode=${response.statusCode}, responseBody={}", response.bodyTo(String::class.java))
                 response.bodyTo(object : ParameterizedTypeReference<List<ChatHistory>>() {})
             }
             ?: emptyList()
